341244b3a7134d229215b3767993c2718850aa4b
Jacob Rowe <jroweschool@gmail.com>
2023-03-20 07:34:27 -0400

    Removed deprecated requirements.txt.

--- src/camera.py
+++ src/camera.py
@@ -3,4 +3,23 @@ import cv2
 
 
 class Camera:
-    cap = cv2.VideoCapture("")
+    def __init__(self, id: str, display=False, **window_size):
+        self.display = display
+
+
+        self.cap = cv2.VideoCapture(id, cv2.CAP_V4L2)
+        self.frame = None
+
+        camera_stream = Thread(target=self.stream_camera)
+        camera_stream.setDaemon(True)
+        camera_stream.start()
+
+    def stream_camera(self):
+        while True:
+            frame = cap.read()
+            
+            if frame is not None:
+                self.frame = frame
+
+                if self.display:
+
